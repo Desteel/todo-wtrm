a0ff0394dc553d99be545b4dd93b4f5107a239e0
Shimba <sh.bumba@gmail.com>
2018-05-06 13:54:55 +0400

    Added styled component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import styled from 'styled-components';
+
+const TestComponent = styled.div`
+  background-color: cornflowerblue;
+  color: white;
+  padding: 1rem;
+  border: 1px solid black;
+`;
 
 class TestApp extends Component {
   render() {
-    return <div>Test App</div>;
+    return <TestComponent>Test App</TestComponent>;
   }
 }
 
