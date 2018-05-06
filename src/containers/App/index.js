3729582f772a79293815ab9445c144ad396a307d
Shimba <sh.bumba@gmail.com>
2018-05-06 17:37:53 +0400

    Add mobx devtools

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {hot} from 'react-hot-loader';
 import {Route, Router, Switch} from 'react-router';
 import {Link} from 'react-router-dom';
 import {TodoApp} from './../ToDo';
 import {NotFound} from "../NotFound";
 
-export const App = hot(module)(({history}) => (
-  <Router history={history}>
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li><Link to='/'>Todo</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path="/" component={TodoApp}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </div>
-  </Router>
-));
+class RootApp extends Component {
+  renderDevTool() {
+    if (process.env.NODE_ENV !== 'production') {
+      const DevTools = require('mobx-react-devtools').default;
+      return <DevTools/>;
+    }
+  }
+
+  render() {
+    const {history} = this.props;
+
+    return <Router history={history}>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li><Link to='/'>Todo</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path="/" component={TodoApp}/>
+          <Route component={NotFound}/>
+        </Switch>
+        {this.renderDevTool()}
+      </div>
+    </Router>;
+  }
+}
+
+export const App = hot(module)(props => <RootApp {...props}/>);
