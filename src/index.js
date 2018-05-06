6b8c1b5da90ddea1fbada0a799b869298c32f1dc
Shimba <sh.bumba@gmail.com>
2018-05-06 13:48:24 +0400

    Add React, Babel and .editorconfig

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello :)');
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+class TestApp extends Component {
+  render() {
+    return <div>Test App</div>;
+  }
+}
+
+render(<TestApp/>, document.getElementById('app'));
