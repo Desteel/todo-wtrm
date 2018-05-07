eb3ea55492844be59cae7e4a88ab24c9bb169e9e
Shimba <sh.bumba@gmail.com>
2018-05-07 09:23:11 +0400

    Create todo

--- src/containers/ToDo/index.tsx
+++ src/containers/ToDo/index.tsx
@@ -1,17 +1,103 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import {inject, observer} from 'mobx-react';
-import {TestComponent} from './style';
 import * as STORE from './../../constants/store';
 
 @inject(STORE.TODO_STORE)
 @observer
 export class TodoApp extends React.Component {
-  render() {
+  static propTypes = {
+    onNewItem: PropTypes.func
+  };
+
+  static defaultProps = {
+    onNewItem: () => {}
+  };
+
+  state = {
+    items: []
+  };
+
+  constructor(props) {
+    super(props);
+
+    if (props.items) {
+      this.setState({
+        items: props.items
+      })
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  toggleRemoveItem(id) {
     const todoStore = this.props[STORE.TODO_STORE];
 
-    return <TestComponent>
-      Todo App ({todoStore.typeString})
-      <button onClick={() => todoStore.toggleType()}>toggle type</button>
-    </TestComponent>;
+    todoStore.toggleRemoveItem(id);
+  };
+
+  addItem(text) {
+    if (text.length < 5) {
+      console.error('Min lenght 5');
+
+      return;
+    }
+
+    const todoStore = this.props[STORE.TODO_STORE];
+
+    const newItem = todoStore.createItem(text);
+
+    todoStore.addItem(newItem);
+  }
+
+  render() {
+    const todoStore = this.props[STORE.TODO_STORE];
+    const items = todoStore.itemsList;
+
+    console.log(items);
+
+    return <div className='todo-items'>
+      <TodoForm onAddItem={ text => { this.addItem(text) } }></TodoForm>
+      <TodoList items={items} onChangeRemove={ id => { this.toggleRemoveItem(id) } }></TodoList>
+    </div>;
   }
 }
+
+const TodoForm = ({ onAddItem = (text) => {} }) => {
+  let _text = '';
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    onAddItem(_text.value);
+
+    _text.value = '';
+
+    _text.focus();
+  };
+
+  return <form className="todo-items__form" onSubmit={submit}>
+    <input ref={ input => _text = input } className="todo-items__input" type="text" placeholder="Input text" required/>
+    <span className="todo-items__submit btn" onClick={submit}>Send</span>
+  </form>
+};
+
+const TodoList = ({ items, onChangeRemove = (id: number) => {} }) => {
+  return <div className="todo-items__list">
+    {items.map(item =>
+      <TodoItem key={item.id} item={item} onChangeRemove={ () => onChangeRemove(item.id) }></TodoItem>)}
+  </div>;
+};
+
+const TodoItem = ({ item: { text, remove, completed }, onChangeRemove = () => {} }) => {
+  return <div className={ 'todo-items__item' + (remove ? ' todo-items__item--deleted' : '') }>
+    <div className="todo-items__col">
+      {text}
+    </div>
+    <div className="todo-items__col">
+      <div className="btn" onClick={onChangeRemove}>{ !remove ? 'delete' : 'back' }</div>
+    </div>
+  </div>;
+};
--- src/stores/todo.tsx
+++ src/stores/todo.tsx
@@ -2,6 +2,7 @@ import {action, computed, observable} from 'mobx';
 
 export class ToDoStore {
   @observable type = false;
+  @observable items = [];
 
   @computed get typeState() {
     return this.type;
@@ -11,9 +12,53 @@ export class ToDoStore {
     return '' + this.type;
   }
 
+  get itemsList() {
+    return this.items;
+  }
+
   @action('toggle type')
   toggleType() {
     this.type = !this.type;
+  }
+
+  uniqueHash() {
+    return ('' + Date.now()).split('').reduce((prev, current) => {
+      return prev + Math.random().toString(34).substring(1, parseInt(current));
+    }, '').replace(/\W+/g, '');
+  }
+
+  @action('create todo item')
+  createItem(text) {
+    return {
+      id: this.uniqueHash(),
+      text,
+      completed: false,
+      remove:false
+    }
+  }
+
+  @action('add new todo element')
+  addItem(item) {
+    this.items = [...this.items, item]
+  }
+
+  getItemById(id) {
+    return this.items.filter(item => item.id === id);
+  }
+
+  @action('toggle remove todo item')
+  toggleRemoveItem(id) {
+    let foundItem = this.getItemById(id);
+
+    if (!foundItem.length) {
+      return;
+    }
+
+    foundItem = foundItem[0];
+
+    foundItem.remove = !foundItem.remove;
+
+    this.items = this.items.map(item => (foundItem.id === item.id ? foundItem : item));
   }
 }
 
